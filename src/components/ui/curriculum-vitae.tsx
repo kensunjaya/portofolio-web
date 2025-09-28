24230faf51bb0a688d4052aeed8971e715fba7e4
Kenneth sunjaya <kennethsunjaya@gmail.com>
2025-09-28 23:46:26 +0700

    Add animations and labels to social media icons; improve CV button layout

--- src/components/ui/curriculum-vitae.tsx
+++ src/components/ui/curriculum-vitae.tsx
@@ -3,13 +3,13 @@ import { MdOutlineOpenInNew } from "react-icons/md";
 
 export const ViewCVButton = () => {
   return (
-    <div className="fixed top-3 md:top-10 right-0 md:right-10 w-fit h-fit z-1000 text-cfgray">
+    <div className="fixed top-3 md:top-10 right-0 md:right-10 w-fit h-fit z-1000">
       <CustomButton
         onClick={() => window.open("https://drive.google.com/file/d/199QgDAVlS0L_EKf9MHZjd0XSXaXPUsYa/view?usp=sharing", "_blank")}
         nofill
         className="font-normal"
       >
-        <div className="flex items-center justify-center gap-2 hover:scale-105 transition-transform duration-200">
+        <div className="flex items-center justify-end gap-2 hover:scale-105 transition-all">
           <MdOutlineOpenInNew />
           <span>Download CV</span>
         </div>
