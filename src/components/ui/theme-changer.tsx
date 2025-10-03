9a48b526b756e303da29bd3870b6b3343e7a6e6e
Kenneth sunjaya <kennethsunjaya@gmail.com>
2025-10-04 00:56:37 +0700

    fix theme button positioning on portrait mode

--- src/components/ui/theme-changer.tsx
+++ src/components/ui/theme-changer.tsx
@@ -5,7 +5,7 @@ import { MdSunny } from "react-icons/md";
 export const ThemeChanger = () => {
   const { isDarkMode, toggleTheme } = useTheme();
   return (
-    <div className="fixed bottom-5 md:bottom-15 left-5 md:left-15 w-fit h-fit z-1000 text-2xl md:text-3xl text-cfgray">
+    <div className="fixed bottom-5 md:bottom-15 landscape:left-5 landscape:md:left-15 portrait:right-5 portrait:md:right-15 w-fit h-fit z-1000 text-2xl md:text-3xl text-cfgray">
       {isDarkMode ? <MdSunny onClick={toggleTheme} className="transition cursor-pointer hover:rotate-30 hover:scale-110 hover:text-secondary" /> : 
                     <FaMoon onClick={toggleTheme} className="transition cursor-pointer hover:-rotate-20 hover:scale-110 hover:text-secondary" />}
     </div>
